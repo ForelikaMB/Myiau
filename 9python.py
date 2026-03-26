f66151ce2811c209d64fd5759e84c86aab03b598
ForelikaMB <andrejmuravin491@gmail.com>
2026-03-26 18:57:31 +0300

    9

--- 9python.py
+++ 9python.py
@@ -45,15 +45,15 @@
 #         cnt+=1
 #         print(cnt)
 
-cnt = 0 
-for i in open('4.txt'):
-    a = [int(x)for x in i.split()]
-    if (a[0]==90 and int(int(a[0])+ int(a[1]) +int(a[-1]))==180) or \
-       (a[1]==90 and int(int(a[0])+ int(a[1]) +int(a[-1]))==180) \
-    or (a[-1]==90 and int(int(a[0])+ int(a[1]) +int(a[-1]))==180):
-        cnt+=1
-        print(a)
-print(cnt)
+# cnt = 0 
+# for i in open('4.txt'):
+#     a = [int(x)for x in i.split()]
+#     if (a[0]==90 and int(int(a[0])+ int(a[1]) +int(a[-1]))==180) or \
+#        (a[1]==90 and int(int(a[0])+ int(a[1]) +int(a[-1]))==180) \
+#     or (a[-1]==90 and int(int(a[0])+ int(a[1]) +int(a[-1]))==180):
+#         cnt+=1
+#         print(a)
+# print(cnt)
 
 # cnt = 0
 # for i in open('4.txt'):
@@ -63,3 +63,86 @@ print(cnt)
 #         cnt+=1
 #         print(a)
 # print(cnt)
+
+# cnt = 0
+# for i in open("tx3.txt"):
+#     a = sorted([int(x) for x in i.split()])
+#     if abs(int(a[-1])-int(a[0]))**3<=(int(a[1])+int(a[2]))**2:
+#         cnt+=1
+# print(cnt)
+
+# zov = set()
+# Odinakovie = []
+# NeOdinacovie = []
+# for i in open('tx4.txt'):
+#     a = sorted([int(x)for x in i.split()])
+#     print(a)
+
+# cnt = 0
+# for i in open('h67.txt'):
+#     a = ([int(x)for x in i.split()])
+#     per = [x for x in a if a.count(x)==3]
+#     penis2 = [x for x in a if a.count(x)==1]
+#     if len(per) == 3 and len(penis2)== 3:
+#         if (sum(per))**2 > (sum(penis2))**2:
+#             cnt+=1
+# print(cnt)
+
+# cnt = 0 
+# for i in open("1.txt"):
+#     a = sorted([int(x)for x in i.split()])
+#     if a[-1]<(a[0]+a[1]+a[2]):
+#         if a[0]+a[3] == a[1]+a[2]:
+#             cnt+=1
+# print(cnt)
+
+# cnt = 0
+# for i in open('1.txt'):
+#     a = sorted([int(x) for x in i.split()])
+#     pre = [x for x in a if a.count(x)==2]
+#     # vto = [x for x in a  if a.count(x)== 2]
+#     tri = [x for x in a if a.count(x)== 1]
+#     if len(pre)== 4 and len(tri)== 3:
+#         if sum(pre)/4 < sum(tri)/3:
+#             cnt+=1
+# print(cnt)
+
+# cnt =0
+# for i in open('1.txt'):
+#     a = [int(x) for x in i.split()]
+#     b = [x for x in a if a.count(x)==2]
+#     c = sorted([x for x in a if a.count(x)==1])
+#     if len(b)==2 and len(c)==5:
+#         if ((c[0])*(c[1])*(c[2]))>(b[0])**2:
+#             cnt+=1
+# print(cnt)
+
+# cnt=0
+# for i in open('1.txt'):
+#     a = sorted([int(x) for x in i.split()])
+#     b = [x for x in a if a.count(x)== 2]
+#     c = [x for x in a if a.count(x)== 1]
+#     if len(c)==2 and len(b)==2:
+#         if ((b[0]%2 ==0) and (b[1]%2 ==0)) and ((c[0]%2!=0) and (c[1]%2!=0)):
+#             cnt+=1
+# print(cnt)
+
+# cnt= 0
+# for i in open('1.txt'):
+#     a = sorted([int(x)for x in i.split()])
+#     b = [x for x in a if a.count(x)==2]
+#     c = sorted([x for x in a if a.count(x)==1])
+#     if len(b)==2 and len(c)==5 and c.count(a[0])==1 and c.count(a[-1])==1:
+#         cnt+=1
+# print(cnt)
+
+cnt=0
+for i in open('1.txt'):
+    a = ([int(x)for x in i.split()])
+    b = sorted([x for x in a if a.count(x)%2==0])
+    if len(b)==len(a)-len(b):
+        if (b[0]+b[2])==(b[1]+b[1])==(b[3]+b[0]):
+                cnt +=1
+print(cnt)
+
+            
